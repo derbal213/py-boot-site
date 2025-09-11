18fe949f3913a1e98981a6812d02b5ac0a176076
Derek <git@derekball.net>
2025-09-11 18:48:39 -0400

    Added function to convert text node to HTML node with asociated tests

---
+++ src/functions/test_text_to_html.py
@@ -0,0 +1,99 @@
+import unittest
+from textnode import TextNode, TextType
+from leafnode import LeafNode
+from text_to_html import text_node_to_html_node
+
+class TestTextToHTML(unittest.TestCase):
+    def test_text(self):
+        node = TextNode("This is a text node", TextType.PLAIN)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, None)
+        self.assertEqual(html_node.value, "This is a text node")
+
+    def test_link(self):
+        node = TextNode("This is a link node", TextType.LINK, "boot.dev")
+        html_node = text_node_to_html_node(node)
+        expected = '<a href="boot.dev">This is a link node</a>'
+        self.assertEqual(html_node.tag, "a")
+        self.assertEqual(html_node.value, "This is a link node")
+        self.assertEqual(html_node.to_html(), expected)
+
+    def test_bold(self):
+        node = TextNode("This is a bold node", TextType.BOLD)
+        html_node = text_node_to_html_node(node)
+        expected = '<b>This is a bold node</b>'
+        self.assertEqual(html_node.tag, "b")
+        self.assertEqual(html_node.value, "This is a bold node")
+        self.assertEqual(html_node.to_html(), expected)
+
+    def test_bold_with_link(self):
+        node = TextNode("This is a bold node", TextType.BOLD, "boot.dev")
+        html_node = text_node_to_html_node(node)
+        expected = '<b>This is a bold node</b>'
+        self.assertEqual(html_node.tag, "b")
+        self.assertEqual(html_node.value, "This is a bold node")
+        self.assertEqual(html_node.to_html(), expected)
+
+    def test_italic(self):
+        node = TextNode("This is an italic node", TextType.ITALIC)
+        html_node = text_node_to_html_node(node)
+        expected = '<i>This is an italic node</i>'
+        self.assertEqual(html_node.tag, "i")
+        self.assertEqual(html_node.value, "This is an italic node")
+        self.assertEqual(html_node.to_html(), expected)
+
+    def test_italic_with_link(self):
+        node = TextNode("This is an italic node", TextType.ITALIC, "boot.dev")
+        html_node = text_node_to_html_node(node)
+        expected = '<i>This is an italic node</i>'
+        self.assertEqual(html_node.tag, "i")
+        self.assertEqual(html_node.value, "This is an italic node")
+        self.assertEqual(html_node.to_html(), expected)
+
+    def test_code(self):
+        node = TextNode("This is a code node", TextType.CODE)
+        html_node = text_node_to_html_node(node)
+        expected = '<code>This is a code node</code>'
+        self.assertEqual(html_node.tag, "code")
+        self.assertEqual(html_node.value, "This is a code node")
+        self.assertEqual(html_node.to_html(), expected)
+
+    def test_code_with_link(self):
+        node = TextNode("This is a code node", TextType.CODE, "boot.dev")
+        html_node = text_node_to_html_node(node)
+        expected = '<code>This is a code node</code>'
+        self.assertEqual(html_node.tag, "code")
+        self.assertEqual(html_node.value, "This is a code node")
+        self.assertEqual(html_node.to_html(), expected)
+
+    def test_img(self):
+        node = TextNode("This is an img node", TextType.IMAGE, "boot.dev")
+        html_node = text_node_to_html_node(node)
+        expected = '<img src="boot.dev" alt="This is an img node" />'
+        self.assertEqual(html_node.tag, "img")
+        #self.assertEqual(html_node.value, "This is an img node")
+        self.assertEqual(html_node.to_html(), expected)
+
+    def test_img_no_text(self):
+        node = TextNode(None, TextType.IMAGE, "boot.dev")
+        html_node = text_node_to_html_node(node)
+        expected = '<img src="boot.dev" />'
+        self.assertEqual(html_node.tag, "img")
+        #self.assertEqual(html_node.value, "This is a img node")
+        self.assertEqual(html_node.to_html(), expected)
+
+    def test_img_no_url(self):
+        node = TextNode("This is an img node", TextType.IMAGE)
+        html_node = text_node_to_html_node(node)
+        expected = '<img alt="This is an img node" />'
+        self.assertEqual(html_node.tag, "img")
+        #self.assertEqual(html_node.value, "This is an img node")
+        self.assertEqual(html_node.to_html(), expected)
+
+    def test_img_just_tag(self):
+        node = TextNode(None, TextType.IMAGE)
+        html_node = text_node_to_html_node(node)
+        expected = '<img />'
+        self.assertEqual(html_node.tag, "img")
+        #self.assertEqual(html_node.value, "This is an img node")
+        self.assertEqual(html_node.to_html(), expected)
---
+++ src/functions/text_to_html.py
@@ -0,0 +1,31 @@
+from textnode import TextNode, TextType
+from htmlnode import HTMLNode
+from parentnode import ParentNode
+from leafnode import LeafNode
+
+def text_node_to_html_node(text_node: TextNode):
+    if text_node is None:
+        return None
+    
+    match text_node.text_type:
+        case TextType.PLAIN:
+            node = LeafNode(None, text_node.text)
+            return node
+        case TextType.BOLD:
+            return LeafNode("b", text_node.text)
+        case TextType.ITALIC:
+            return LeafNode("i", text_node.text)
+        case TextType.CODE:
+            return LeafNode("code", text_node.text)
+        case TextType.LINK:
+            return LeafNode("a", text_node.text, {"href":text_node.url})
+        case TextType.IMAGE:
+            props = {}
+            if text_node.url is not None:
+                props["src"] = text_node.url
+            if text_node.text is not None:
+                props["alt"] = text_node.text
+            if len(props) == 0: props = None
+            return LeafNode("img", "", props)
+        case _:
+            raise ValueError("Text type does not match an HTML tag")
