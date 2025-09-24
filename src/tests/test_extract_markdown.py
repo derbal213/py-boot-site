42cebf544ab95e0632937c9cea98cdf1d5713eea
Derek <git@derekball.net>
2025-09-24 12:13:47 -0400

    test refactoring

--- src/tests/test_extract_markdown.py
+++ src/tests/test_extract_markdown.py
@@ -55,113 +55,81 @@ across two lines```
         self.assertListEqual([("image", "https://i.imgur.com/zjjcJKZ.png")], matches)
 
     def test_extract_markdown_links(self):
-        text = "This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)"
-        matches = extract_markdown_links(text)
-        expected = [("to boot dev", "https://www.boot.dev"), ("to youtube", "https://www.youtube.com/@bootdotdev")]
-        self.assertListEqual(expected, matches)
+        matches = extract_markdown_links("This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)")
+        self.assertListEqual(
+            [("to boot dev", "https://www.boot.dev"), ("to youtube", "https://www.youtube.com/@bootdotdev")], 
+            matches
+        )
 
     def test_extract_multiple_images(self):
-        text = "![one](url1) and ![two](url2)"
-        expected = [("one", "url1"), ("two", "url2")]
-        matches = extract_markdown_images(text)
-        self.assertListEqual(expected, matches)
+        matches = extract_markdown_images("![one](url1) and ![two](url2)")
+        self.assertListEqual([("one", "url1"), ("two", "url2")], matches)
         
     def test_extract_malformed_image(self):
         matches = extract_markdown_images(
             "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png"
         )
         self.assertListEqual([], matches)
-        #self.assertListEqual([("image", "https://i.imgur.com/zjjcJKZ.png")], matches)
 
     def test_extract_single_link(self):
-        text = "[one](url1)"
-        expected = [("one", "url1")]
-        matches = extract_markdown_links(text)
-        self.assertListEqual(expected, matches)
+        matches = extract_markdown_links("[one](url1)")
+        self.assertListEqual([("one", "url1")], matches)
 
     def test_extract_spaced_txt_image(self):
-        text = "![my cool image](http://img.com/cool.png)"
-        expected = [("my cool image", "http://img.com/cool.png")]
-        matches = extract_markdown_images(text)
-        self.assertListEqual(expected, matches)
+        matches = extract_markdown_images("![my cool image](http://img.com/cool.png)")
+        self.assertListEqual([("my cool image", "http://img.com/cool.png")], matches)
     
     def test_extract_no_images(self):
-        text = "This text has no markdown image"
-        expected = []
-        matches = extract_markdown_images(text)
-        self.assertListEqual(expected, matches)
+        matches = extract_markdown_images("This text has no markdown image")
+        self.assertListEqual([], matches)
 
     def test_extract_img_in_sentence(self):
-        text = "Check this out (![wow](url))."
-        expected = [("wow", "url")]
-        matches = extract_markdown_images(text)
-        self.assertListEqual(expected, matches)
+        matches = extract_markdown_images("Check this out (![wow](url)).")
+        self.assertListEqual([("wow", "url")], matches)
 
     def test_extract_img_empty_txt(self):
-        text = "![](http://example.com/no-alt.png)"
-        expected = [("", "http://example.com/no-alt.png")]
-        matches = extract_markdown_images(text)
-        self.assertListEqual(expected, matches)
+        matches = extract_markdown_images("![](http://example.com/no-alt.png)")
+        self.assertListEqual([("", "http://example.com/no-alt.png")], matches)
 
     def test_extract_img_empty_url(self):
-        text = "![alt]()"
-        expected = [("alt", "")]
-        matches = extract_markdown_images(text)
-        self.assertListEqual(expected, matches)
+        matches = extract_markdown_images("![alt]()")
+        self.assertListEqual([("alt", "")], matches)
 
     def test_extract_img_empty(self):
-        text = "![]()"
-        expected = [("", "")]
-        matches = extract_markdown_images(text)
-        self.assertListEqual(expected, matches)
+        matches = extract_markdown_images("![]()")
+        self.assertListEqual([("", "")], matches)
 
     def test_extract_spaced_txt_link(self):
-        text = "[my cool link](http://img.com/cool.png)"
-        expected = [("my cool link", "http://img.com/cool.png")]
-        matches = extract_markdown_links(text)
-        self.assertListEqual(expected, matches)
+        matches = extract_markdown_links("[my cool link](http://img.com/cool.png)")
+        self.assertListEqual([("my cool link", "http://img.com/cool.png")], matches)
     
     def test_extract_no_links(self):
-        text = "This text has no links"
-        expected = []
-        matches = extract_markdown_links(text)
-        self.assertListEqual(expected, matches)
+        matches = extract_markdown_links("This text has no links")
+        self.assertListEqual([], matches)
 
     def test_extract_link_in_sentence(self):
-        text = "Check this out ([wow](url))."
-        expected = [("wow", "url")]
-        matches = extract_markdown_links(text)
-        self.assertListEqual(expected, matches)
+        matches = extract_markdown_links("Check this out ([wow](url)).")
+        self.assertListEqual([("wow", "url")], matches)
 
     def test_extract_link_empty_txt(self):
-        text = "[](http://example.com/no-alt.png)"
-        expected = [("", "http://example.com/no-alt.png")]
-        matches = extract_markdown_links(text)
-        self.assertListEqual(expected, matches)
+        matches = extract_markdown_links("[](http://example.com/no-alt.png)")
+        self.assertListEqual([("", "http://example.com/no-alt.png")], matches)
 
     def test_extract_link_empty_url(self):
-        text = "[alt]()"
-        expected = [("alt", "")]
-        matches = extract_markdown_links(text)
-        self.assertListEqual(expected, matches)
+        matches = extract_markdown_links("[alt]()")
+        self.assertListEqual([("alt", "")], matches)
 
     def test_extract_link_empty(self):
-        text = "[]()"
-        expected = [("", "")]
-        matches = extract_markdown_links(text)
-        self.assertListEqual(expected, matches)
+        matches = extract_markdown_links("[]()")
+        self.assertListEqual([("", "")], matches)
 
     def test_extract_image_not_link(self):
-        text = "Check this out ([wow](url))."
-        expected = []
-        matches = extract_markdown_images(text)
-        self.assertListEqual(expected, matches)
+        matches = extract_markdown_images("Check this out ([wow](url)).")
+        self.assertListEqual([], matches)
 
     def test_extract_link_not_img(self):
-        text = "Check this out (![wow](url))."
-        expected = []
-        matches = extract_markdown_links(text)
-        self.assertListEqual(expected, matches)
+        matches = extract_markdown_links("Check this out (![wow](url)).")
+        self.assertListEqual([], matches)
 
     def test_extract_both(self):
         text = "This is a [sentence](url) with both [links](url2) and images! ![cool](http://img.com/cool.png) ![happy](http://img.com/happy.png)"
@@ -173,13 +141,11 @@ across two lines```
         self.assertListEqual(expected_imgs, matched_imgs)
 
     def test_extract_title_simple(self):
-        text = "# Hello"
-        title = extract_title(text)
+        title = extract_title("# Hello")
         self.assertEqual("Hello", title)
 
     def test_extract_title_fail(self):
-        text = "## Hello"
-        title = extract_title(text)
+        title = extract_title("## Hello")
         self.assertEqual("Default Title", title)
 
     def test_extract_title_buried(self):
